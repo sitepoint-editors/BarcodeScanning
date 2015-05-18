c296cbaef95867308fc1741cc835608d2558fcd7
Jordan Morgan <info@dreaminginbinary.co>
2015-05-17 19:16:54 -0500

    Barcode Scanner Starter Project
    The starter project for the barcode scanning tutorial found on
    sitepoint.com in the mobile section.

--- BarcodeScanning/ViewController.swift
+++ BarcodeScanning/ViewController.swift
@@ -11,14 +11,19 @@ import AVFoundation
 
 class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate
 {
-
+    /// Helper property and not directly used. The camera layer's bounds will be set to this.
     @IBOutlet weak var cameraView: UIView!
+    /// Will show the type of metadata being displayed.
     @IBOutlet weak var lblDataType: UILabel!
+    /// Will show the information from the capture metadata.
     @IBOutlet weak var lblDataInfo: UILabel!
     
     //MARK: Properties
+    /// Runs the capture session.
     let captureSession = AVCaptureSession()
+    /// The device used as input for the capture session.
     var captureDevice:AVCaptureDevice?
+    /// The UI layer to display the feed from the input source, in our case, the camera.
     var captureLayer:AVCaptureVideoPreviewLayer?
     
     //MARK: View lifecycle
@@ -30,69 +35,48 @@ class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate
     override func viewDidAppear(animated: Bool)
     {
         super.viewDidAppear(animated)
-        self.setupCaptureSession()
     }
     
     //MARK: Session Startup
+    /**
+    Begins the capture session.
+    */
     private func setupCaptureSession()
     {
-        self.captureDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
-        var setupError:NSError?
-        
-        if let deviceInput = AVCaptureDeviceInput.deviceInputWithDevice(self.captureDevice, error: &setupError) as? AVCaptureDeviceInput
-        {
-            //Add the input feed to the session and start it
-            self.captureSession.addInput(deviceInput)
-            self.setupPreviewLayer({
-                self.captureSession.startRunning()
-                self.addMetaDataCaptureOutToSession()
-            })
-        }
-        else
-        {
-            self.showError(setupError!.localizedDescription)
-        }
-
+    
     }
     
+    /**
+    Handles setting up the UI to show the camera feed.
+    
+    :param: completion Completion handler to invoke if setting up the feed was successful.
+    */
     private func setupPreviewLayer(completion:() -> ())
     {
-        self.captureLayer = AVCaptureVideoPreviewLayer.layerWithSession(self.captureSession) as? AVCaptureVideoPreviewLayer
         
-        if let capLayer = self.captureLayer
-        {
-            capLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
-            capLayer.frame = self.cameraView.frame
-            self.view.layer.addSublayer(capLayer)
-            completion()
-        }
-        else
-        {
-            self.showError("An error occured beginning video capture.")
-        }
     }
     
     //MARK: Metadata capture
+    /**
+    Handles identifying what kind of data output we want from the session, in our case, metadata and the available types of metadata.
+    */
     private func addMetaDataCaptureOutToSession()
     {
-        let metadata = AVCaptureMetadataOutput()
-        self.captureSession.addOutput(metadata)
-        metadata.metadataObjectTypes = metadata.availableMetadataObjectTypes
-        metadata.setMetadataObjectsDelegate(self, queue: dispatch_get_main_queue())
+        
     }
     
     //MARK: Delegate Methods
     func captureOutput(captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [AnyObject]!, fromConnection connection: AVCaptureConnection!)
     {
-        for metaData in metadataObjects
-        {
-            let decodedData:AVMetadataMachineReadableCodeObject = metaData as! AVMetadataMachineReadableCodeObject
-            self.lblDataInfo.text = decodedData.stringValue
-            self.lblDataType.text = decodedData.type
-        }
+        
     }
     
     //MARK: Utility Functions
+    /**
+    Shows any error that may occur via an alert view.
+    
+    :param: error The error message.
+    */
     private func showError(error:String)
     {
         let alertController = UIAlertController(title: "Error", message: error, preferredStyle: .Alert)
